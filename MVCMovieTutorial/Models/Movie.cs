fe71a5a20d71bb48b46a78acdabc396b7324602f
Allen Olsen <allenolsen@quickenloans.com>
2019-03-21 08:36:43 -0400

    Finished 'Controller actions and views'

--- MVCMovieTutorial/Models/Movie.cs
+++ MVCMovieTutorial/Models/Movie.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Data;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVCMovieTutorial.Models
 {
@@ -9,10 +9,11 @@ namespace MVCMovieTutorial.Models
         public int Id { get; set; }
         public string Title { get; set; }
 
-
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
