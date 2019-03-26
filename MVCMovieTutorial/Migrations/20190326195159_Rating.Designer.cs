639f8f959ad8ea94282b7d87ed842b5fbb4b4bfb
Allen Olsen <allenolsen@quickenloans.com>
2019-03-26 16:15:21 -0400

    Finished 'Add a new field'

--- MVCMovieTutorial/Migrations/20190326195159_Rating.Designer.cs
+++ MVCMovieTutorial/Migrations/20190326195159_Rating.Designer.cs
@@ -4,14 +4,16 @@ using MVCMovieTutorial.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVCMovieTutorial.Migrations
 {
     [DbContext(typeof(MVCMovieContext))]
-    partial class MVCMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20190326195159_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
